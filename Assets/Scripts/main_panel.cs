9206955705fa6f25085af816d203360c506e72a9
Jonas <jonas.verhenne@student.howest.be>
2020-11-17 18:02:18 +0100

    Show detail panels
    hide main panel when clicked on a panel and show corresponding detail panel

---
+++ Assets/Scripts/main_panel.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class main_panel : MonoBehaviour
+{
+    public List<GameObject> panels;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnMouseDown()
+    {
+        //Destroy(gameObject);
+    }
+
+    public void HideMainPanel()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void ShowMainPanel()
+    {
+        gameObject.SetActive(true);
+    }
+}
