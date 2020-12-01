e41997599107a285d834bb5512adcbbf34e7e83d
StijnVandendriessche1 <stijn.vandendriessche3@student.howest.be>
2020-12-01 12:05:40 +0100

    Avatar werkend en verbinden met kaartview
    AvatarMaker toegevoegd
    
    SceneSwitcher toegevoegd met functie die alle opgegeven GameObjecten unabled zet, en het gameobject dat het mee krijgt enabled zet
    
    Button switch nieuwe sprite gegeven
    
    Alles wat achteruit gezet

---
+++ Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public List<GameObject> objects = new List<GameObject>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void switchToScene(GameObject scene)
+    {
+        foreach(GameObject go in objects)
+        {
+            go.SetActive(false);
+        }
+        scene.SetActive(true);
+    }
+}
