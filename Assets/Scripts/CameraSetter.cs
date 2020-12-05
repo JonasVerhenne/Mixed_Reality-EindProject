6c3819dd4bce919fcb7910890b91d2f5a4d7d2de
StijnVandendriessche1 <stijn.vandendriessche3@student.howest.be>
2020-12-05 17:36:16 +0100

    already working some more, panel is not clickable

---
+++ Assets/Scripts/CameraSetter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSetter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Awake()
+    {
+        gameObject.GetComponent<Canvas>().worldCamera = Camera.main;
+        Debug.Log("Camera updated for" + gameObject.ToString());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
